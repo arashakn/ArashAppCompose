1974b420ed080c4a1d51f15d8de71b4d0ab8c140
arashakn <arash.akn@gmail.com>
2022-03-03 14:08:44 -0800

    clean up

--- app/src/main/java/com/arash/arashappcompose/MainViewModel.kt
+++ app/src/main/java/com/arash/arashappcompose/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.arash.arashappcompose
 
 import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.arash.arashapp.network.models.Article
 import com.arash.arashapp.network.responsehandler.NetworkResultWrapper
 import com.arash.arashapp.repositories.SearchRepository
 import com.arash.arashappcompose.repositories.SearchRepositoryImpl
@@ -15,6 +18,13 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(private val repository: SearchRepositoryImpl
 ) : ViewModel(){
     val curVal : MutableLiveData<Int> = MutableLiveData(0)
+    private val _articles = mutableStateOf<List<Article>>(emptyList())
+    val articles: State<List<Article>> = _articles
+    
+    fun setArticles(a: List<Article>) {
+        _articles.value = a
+    }
+    
     init {
         search()
     }
@@ -23,7 +33,11 @@ class MainViewModel @Inject constructor(private val repository: SearchRepository
         viewModelScope.launch {
             val result = repository.search("russia")
             when(result){
-                is NetworkResultWrapper.Success -> Log.d("searchresult", "sucess")
+                is NetworkResultWrapper.Success -> {Log.d("searchresult", "sucess")
+                    result.value.response?.let {
+                        setArticles(it.articles)
+                    }
+                }
                 is NetworkResultWrapper.NetworkError -> Log.d("searchresult", "error")
             }
         }
